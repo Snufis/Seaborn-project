cd4cec8bda34fe111c94b536d8eb0b081b26676d
Snufis <Snufis@protonmail.com>
2026-03-15 18:34:15 +0100

    added new plots, heatmap,violin,pairplot for later

--- main.py
+++ main.py
@@ -47,3 +47,73 @@ plt.title("Porownanie rozkładów wynikow")
 plt.tight_layout()
 plt.savefig("charts/02-kde-porownanie.png")
 plt.show()
+
+
+
+plt.figure(figsize=(8,6))
+
+#Matematyka vs Czytanie (podzial wg płci)
+sns.scatterplot(
+    data=df,
+    x="math_score",
+    y="reading_score",
+    hue="gender",
+    palette={
+            "male":"#4C72B0",
+            "female":"#F39112"
+            }
+)
+
+plt.title("Matematyka vs Czytanie(gender)")
+plt.xlabel("Wynik z matematyki")
+plt.ylabel("Wynik z czytania")
+plt.tight_layout()
+plt.savefig("charts/03-scatter-math-reading.png")
+plt.show()
+
+plt.figure(figsize=(16,6))
+
+#wynik przez płeć boxploty
+plt.subplot(1,3,1)
+sns.boxplot(data=df, x="gender", y="math_score")
+plt.title("Matematyka")
+
+plt.subplot(1,3,2)
+sns.boxplot(data=df, x="gender", y="reading_score")
+plt.title("Czytanie")
+
+plt.subplot(1,3,3)
+sns.boxplot(data=df, x="gender", y="writing_score")
+plt.title("Pisanie")
+
+plt.tight_layout()
+plt.savefig("charts/04-boxplot-gender.png")
+plt.show()
+
+
+plt.figure(figsize=(7,5))
+
+#wpływ kursu przygotow.
+sns.barplot(
+    data=df,
+    x="test_preparation_course",
+    y="math_score",
+    palette="Set2"
+)
+plt.title("Wpływ kursu przygotowawczego na wynik z matematyki")
+plt.xlabel("Kurs przygotowawczy")
+plt.ylabel("Średni wynik")
+
+plt.tight_layout()
+plt.savefig("charts/05-barplot-test-prep.png")
+plt.show()
+
+### LATER
+sns.heatmap(df[["math_score","reading_score","writing_score"]].corr(), annot=True)
+plt.show()
+
+sns.violinplot(data=df, x="gender", y="math_score")
+plt.show()
+
+sns.pairplot(df, hue="gender")
+plt.show()
